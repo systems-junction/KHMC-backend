ef817994f5ca4b9d82c6fe77c14669b5128e8104
Abdul Hannan Butt <hannanbutt1995@gmail.com>
2020-09-03 21:53:44 +0500

    account scenario change

--- controllers/receiveItem.js
+++ controllers/receiveItem.js
@@ -23,30 +23,100 @@ exports.addReceiveItem = asyncHandler(async (req, res) => {
     const { itemId,currentQty, requestedQty, receivedQty, bonusQty, batchNumber,lotNumber,
         expiryDate,unit, discount, unitDiscount, discountAmount, tax, taxAmount, finalUnitPrice, subTotal, 
         discountAmount2,totalPrice, invoice, dateInvoice,dateReceived, notes,materialId,vendorId,prId,status } = req.body;
-            await ReceiveItem.create({
-                itemId,
-                currentQty,
-                requestedQty,
-                receivedQty,
-                bonusQty,
-                batchNumber,
-                lotNumber,
-                expiryDate,
-                unit,
-                discount,
-                unitDiscount,
-                discountAmount,
-                tax,
-                taxAmount,
-                finalUnitPrice,
-                subTotal,
-                discountAmount2,
-                totalPrice,
-                invoice,
-                dateInvoice,
-                dateReceived,
-                notes,prId,status
-            });    
+        var isafter = moment(req.body.dateReceived).isAfter(req.body.expiryDate);
+            if (isafter)
+            {
+               const err =  await ExternalReturnRequest.create({
+                    returnRequestNo: uuidv4(),
+                    generatedBy:"System",
+                    generated:"System",
+                    dateGenerated:req.body.dateReceived,
+                    expiryDate:req.body.expiryDate,
+                    itemId:req.body.itemId,
+                    currentQty:req.body.qty,
+                    description:"Date Expired",
+                    reason:"Expired",
+                    reasonDetail:"Date Expired",
+                    status:"approved",
+                    prId:req.body.prId
+                })
+                notification("Item Date Expired ", "A new Return Request "+err.returnRequestNo+" has been generated at "+err.createdAt+" by System", "Warehouse Incharge")
+                const send = await ExternalReturnRequest.find().populate('itemId');
+                globalVariable.io.emit("get_data", send)
+            }   
+            if(!isafter){
+                if(req.body.receivedQty>req.body.requestedQty)
+                {
+                var qty=req.body.receivedQty-req.body.requestedQty;    
+                await ReceiveItem.create({
+                    itemId,
+                    currentQty,
+                    requestedQty,
+                    receivedQty:req.body.requestedQty,
+                    bonusQty,
+                    batchNumber,
+                    lotNumber,
+                    expiryDate,
+                    unit,
+                    discount,
+                    unitDiscount,
+                    discountAmount,
+                    tax,
+                    taxAmount,
+                    finalUnitPrice,
+                    subTotal,
+                    discountAmount2,
+                    totalPrice,
+                    invoice,
+                    dateInvoice,
+                    dateReceived,
+                    notes,prId,status
+                });
+                await ExternalReturnRequest.create({
+                    returnRequestNo: uuidv4(),
+                    generatedBy:"System",
+                    generated:"System",
+                    dateGenerated:req.body.dateReceived,
+                    expiryDate:req.body.expiryDate,
+                    itemId:req.body.itemId,
+                    currentQty:qty,
+                    description:"Extra quantity",
+                    reason:"Extra quantity",
+                    reasonDetail:"Extra quantity arrived than requested",
+                    status:"approved",
+                    prId:req.body.prId
+                })
+                notification("Extra Quantity Returned", "A new Return Request "+err.returnRequestNo+" has been generated at "+err.createdAt+" by System", "Warehouse Incharge")
+                const send = await ExternalReturnRequest.find().populate('itemId');
+                globalVariable.io.emit("get_data", send)
+                }
+                else{
+                    await ReceiveItem.create({
+                        itemId,
+                        currentQty,
+                        requestedQty,
+                        receivedQty,
+                        bonusQty,
+                        batchNumber,
+                        lotNumber,
+                        expiryDate,
+                        unit,
+                        discount,
+                        unitDiscount,
+                        discountAmount,
+                        tax,
+                        taxAmount,
+                        finalUnitPrice,
+                        subTotal,
+                        discountAmount2,
+                        totalPrice,
+                        invoice,
+                        dateInvoice,
+                        dateReceived,
+                        notes,prId,status
+                    });
+                }  
+            }    
     const prapp =  await PurchaseRequest.findOneAndUpdate({'_id': prId, 'item.itemId': itemId},{ $set: { 'item.$.status': status }},{new: true});
     var count =0;
     for(let i = 0 ; i<prapp.item.length; i++)
@@ -123,79 +193,7 @@ exports.addReceiveItem = asyncHandler(async (req, res) => {
 //                 const send = await ExternalReturnRequest.find().populate('itemId');
 //                 globalVariable.io.emit("get_data", send)
 //             }
-//             if(!isafter){
-//         if(req.body.receivedQty>req.body.requestedQty)
-//         {
-//         var qty=req.body.receivedQty-req.body.requestedQty;    
-//         await ReceiveItem.create({
-//             itemId,
-//             currentQty,
-//             requestedQty,
-//             receivedQty:req.body.requestedQty,
-//             bonusQty,
-//             batchNumber,
-//             lotNumber,
-//             expiryDate,
-//             unit,
-//             discount,
-//             unitDiscount,
-//             discountAmount,
-//             tax,
-//             taxAmount,
-//             finalUnitPrice,
-//             subTotal,
-//             discountAmount2,
-//             totalPrice,
-//             invoice,
-//             dateInvoice,
-//             dateReceived,
-//             notes,prId,status
-//         });
-//         await ExternalReturnRequest.create({
-//             returnRequestNo: uuidv4(),
-//             generatedBy:"System",
-//             generated:"System",
-//             dateGenerated:req.body.dateReceived,
-//             expiryDate:req.body.expiryDate,
-//             itemId:req.body.itemId,
-//             currentQty:qty,
-//             description:"Extra quantity",
-//             reason:"Extra quantity",
-//             reasonDetail:"Extra quantity arrived than requested",
-//             status:"approved",
-//             prId:req.body.prId
-//         })
-//         notification("Extra Quantity Returned", "A new Return Request "+err.returnRequestNo+" has been generated at "+err.createdAt+" by System", "Warehouse Incharge")
-//         const send = await ExternalReturnRequest.find().populate('itemId');
-//         globalVariable.io.emit("get_data", send)
-//         }
-//         else{
-//             await ReceiveItem.create({
-//                 itemId,
-//                 currentQty,
-//                 requestedQty,
-//                 receivedQty,
-//                 bonusQty,
-//                 batchNumber,
-//                 lotNumber,
-//                 expiryDate,
-//                 unit,
-//                 discount,
-//                 unitDiscount,
-//                 discountAmount,
-//                 tax,
-//                 taxAmount,
-//                 finalUnitPrice,
-//                 subTotal,
-//                 discountAmount2,
-//                 totalPrice,
-//                 invoice,
-//                 dateInvoice,
-//                 dateReceived,
-//                 notes,prId,status
-//             });
-//         }  
-//     }
+          
     
 //     await PurchaseRequest.findOneAndUpdate({'_id': prId},{ $set: { status: 'pending_approval_from_accounts' }},{new: true});
 //     const mat = await MaterialReceiving.findOneAndUpdate({'_id': materialId,'prId.id':prId},{ $set: { 'prId.$.status': req.body.status }},{new: true});
