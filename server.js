4d9d238163ae20c071aff0ded23e4f972f10496f
Abdul Hannan Butt <hannanbutt1995@gmail.com>
2020-09-21 19:54:21 +0500

    live db

--- server.js
+++ server.js
@@ -19,13 +19,13 @@ var start = new Date(now.getFullYear(), 0, 0);
 var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
 var oneDay = 1000 * 60 * 60 * 24;
 var day = Math.floor(diff / oneDay);
-// const db = require('monk')(
-//   'mongodb+srv://khmc:khmc12345@khmc.r3oxo.mongodb.net/stagingdb?retryWrites=true&w=majority'
-// );
-
 const db = require('monk')(
-  'mongodb+srv://khmc:khmc12345@khmc.r3oxo.mongodb.net/test?retryWrites=true&w=majority'
+  'mongodb+srv://khmc:khmc12345@khmc.r3oxo.mongodb.net/stagingdb?retryWrites=true&w=majority'
 );
+
+// const db = require('monk')(
+//   'mongodb+srv://khmc:khmc12345@khmc.r3oxo.mongodb.net/test?retryWrites=true&w=majority'
+// );
 dotenv.config({ path: './config/.env' });
 connectDB();
 var transporter = nodemailer.createTransport({
