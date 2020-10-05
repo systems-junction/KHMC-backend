1a931fa291101b046834c828bce7450ed2f8693c
Abdul Hannan Butt <hannanbutt1995@gmail.com>
2020-10-05 12:47:20 +0500

    cpt categories differentiation

--- controllers/codes.js
+++ controllers/codes.js
@@ -8,6 +8,26 @@ exports.getCPT = asyncHandler(async (req, res) => {
   res.status(200).json({ success: true, data: cpt });
 });
 
+exports.getCPTCategories = asyncHandler(async (req, res) => {
+  const cpt = await CPT.find().select({procedureCodeCategory:1})
+  var uniqueArray = (function(cpt){
+      var m = {}, uniqueArray = []
+      for (var i=0; i<cpt.length; i++) {
+        var v = cpt[i].procedureCodeCategory;
+        if (!m[v]) {
+          uniqueArray.push(v);
+          m[v]=true;
+        }
+      }
+      return uniqueArray;
+    })(cpt);
+  res.status(200).json({ success: true, data: uniqueArray });
+});
+exports.getCPTByCategories = asyncHandler(async (req, res) => {
+  const cpt = await CPT.find({procedureCodeCategory:req.params.code}).select({cptCodes:1,procedureCodeDescriptions:1})
+  res.status(200).json({ success: true, data: cpt });
+});
+
 exports.getCPTById = asyncHandler(async (req, res) => {
     const cpt = await CPT.findOne({_id:req.params.id})
     res.status(200).json({ success: true, data: cpt });
--- routes/codes.js
+++ routes/codes.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const {
     getCPT,
+    getCPTCategories,
+    getCPTByCategories,
     getCPTById,
     getICD,
-    getICDById,
     getICDCategories,
-    getICDByCategories
+    getICDByCategories,
+    getICDById
 } = require('../controllers/codes');
 
 const router = express.Router();
 router.get('/getcpt', getCPT);
+router.get('/getcptcat', getCPTCategories);
+router.get('/getcptcat/:code', getCPTByCategories);
 router.get('/getcpt/:id', getCPTById);
 router.get('/geticd', getICD);
 router.get('/geticdcat', getICDCategories);
