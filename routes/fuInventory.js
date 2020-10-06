dca99c7e79238bae0e71ad8d1ae643669fd60be3
Abdul Hannan Butt <hannanbutt1995@gmail.com>
2020-10-06 15:00:08 +0500

    test

--- routes/fuInventory.js
+++ routes/fuInventory.js
@@ -13,10 +13,10 @@ const {
 const router = express.Router();
 
 
-router.get('/getfuinventory', getFuInventory);
-router.get('/getfuinventory/:keyword', getFuInventoryKeyword);
-router.get('/getfuinventorybyfu/:_id', getFuInventoryByFU);
-router.get('/getfuinventorybyfu/:_id/:keyword', getFuInventoryByFUKeyword);
+// router.get('/getfuinventory', getFuInventory);
+// router.get('/getfuinventory/:keyword', getFuInventoryKeyword);
+router.get('/getfuinventory/:_id', getFuInventoryByFU);
+router.get('/getfuinventory/:_id/:keyword', getFuInventoryByFUKeyword);
 router.post('/addfuinventory', addFuInventory);
 router.delete('/deletefuinventory/:_id', deleteFuInventory);
 router.put('/updatefuinventory', updateFuInventory);
