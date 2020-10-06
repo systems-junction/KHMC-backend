713ceaf92115f8037cca169adb758f505188ee8b
Abdul Hannan Butt <hannanbutt1995@gmail.com>
2020-10-06 14:32:46 +0500

    fuinventory by fu keyworc

--- routes/fuInventory.js
+++ routes/fuInventory.js
@@ -3,6 +3,7 @@ const {
     getFuInventory,
     getFuInventoryKeyword,
     getFuInventoryByFU,
+    getFuInventoryByFUKeyword,
     addFuInventory,
     deleteFuInventory,
     updateFuInventory,
@@ -15,6 +16,7 @@ const router = express.Router();
 router.get('/getfuinventory', getFuInventory);
 router.get('/getfuinventory/:keyword', getFuInventoryKeyword);
 router.get('/getfuinventory/:_id', getFuInventoryByFU);
+router.get('/getfuinventory/:_id/:keyword', getFuInventoryByFUKeyword);
 router.post('/addfuinventory', addFuInventory);
 router.delete('/deletefuinventory/:_id', deleteFuInventory);
 router.put('/updatefuinventory', updateFuInventory);
