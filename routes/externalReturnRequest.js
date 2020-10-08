67ed77a191305f6df43e0d079d9c13680e18b08c
Abdul Hannan Butt <hannanbutt1995@gmail.com>
2020-10-08 17:28:26 +0500

    external keyword

--- routes/externalReturnRequest.js
+++ routes/externalReturnRequest.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {
     getExternalReturnRequests,
+    getExternalReturnRequestsKeyword,
     getExternalReturnRequestsById,
     deleteExternalReturnRequests,
     addExternalReturnRequest,
@@ -9,6 +10,7 @@ const {
 
 const router = express.Router();
 router.get('/getexternalreturnrequests', getExternalReturnRequests);
+router.get('/getexternalreturnrequests/:keyword', getExternalReturnRequestsKeyword);
 router.get('/getexternalreturnrequest/:_id', getExternalReturnRequestsById);
 router.delete('/deleteexternalreturnrequest/:_id', deleteExternalReturnRequests);
 router.post('/addexternalreturnrequest', addExternalReturnRequest);
