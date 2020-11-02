a4301a7627a51a89bdcdc13ff8bbc2cf262cb468
Mudassir Ijaz <ijazmudassir786@gmail.com>
2020-11-02 17:38:35 +0500

    reset password

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,9 @@ const { validateParams } = require('../middleware/validator');
 const {
   register,
   login,
-  logout
+  logout,
+  resetPassword,
+  changePassword,
 } = require('../controllers/auth');
 
 const router = express.Router();
@@ -11,20 +13,26 @@ const router = express.Router();
 // const { protect } = require('../middleware/auth');
 
 router.post('/register', register);
-router.post('/login', validateParams([
-  {
+router.post(
+  '/login',
+  validateParams([
+    {
       param_key: 'email',
       required: true,
       type: 'string',
       // validator_functions: [(param) => {return param.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)}]
-  },
-  {
+    },
+    {
       param_key: 'password',
       required: true,
       type: 'string',
       // validator_functions: [(param) => {return param.length === 6}]
-  }
-]), login);
+    },
+  ]),
+  login
+);
 router.get('/logout', logout);
+router.post('/resetpassword', resetPassword);
+router.post('/passwordchange', changePassword);
 
 module.exports = router;
